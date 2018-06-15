be69155ac75a6d2df86b97953320bfdb25522e1a
Naduysha1234 <churaeva1234@gmail.com>
2018-06-15 19:32:27 +0300

    init commit

---
+++ src/main/java/com/spring/project/projectmemories/ProjectMemoriesApplication.java
@@ -0,0 +1,12 @@
+package com.spring.project.projectmemories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMemoriesApplication.class, args);
+	}
+}
